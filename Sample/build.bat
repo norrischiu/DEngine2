1438e5ac6942de70e54e7be533c6e8d047d54307
norrischiu <sungchiu@usc.edu>
2019-08-28 23:57:01 +0900

    Build DExporter and assets at sample build script

--- Sample/build.bat
+++ Sample/build.bat
@@ -1,2 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
 cd %~dp0
+
+echo Building solution...
 ..\DTools\External\premake5 vs2017
+
+echo Build DExporter.exe...
+for /f "usebackq tokens=*" %%i in (`..\DTools\External\vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+  "%%i" ..\DTools\DExporter\DExporter.vcxproj -p:PlatformToolset=v141 -p:Platform=x64 -t:Build -p:Configuration=Release
+)
+
+echo Copying external libraries...
+xcopy /y ..\DTools\External\Assimp\bin\x64\assimp-vc141-mt.dll  ..\DTools\Bin\
+
+echo Building assets...
+..\DTools\Bin\DExporterRelease.exe model Assets\Models\ShaderBall\shader_ball.obj Bin\Assets\ ShaderBall
+
+echo Done
+
+pause
