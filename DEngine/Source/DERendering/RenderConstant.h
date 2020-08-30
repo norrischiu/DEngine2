93d0a6947833c205e95c22af1539e1744622aceb
Norris Chiu <norrisust@gmail.com>
2020-08-31 02:55:30 +0900

    Create isosphere for point light debug mesh, update sample code

--- DEngine/Source/DERendering/RenderConstant.h
+++ DEngine/Source/DERendering/RenderConstant.h
@@ -1,52 +1,166 @@
 #pragma once
 
+#include <DECore/Container/Vector.h>
+#include <math.h>
+
 namespace DE
 {
 
 struct RenderConstant
 {
-static constexpr float CubeVertices[] = {
-    -1.0f,  1.0f, -1.0f,
-    -1.0f, -1.0f, -1.0f,
-     1.0f, -1.0f, -1.0f,
-     1.0f, -1.0f, -1.0f,
-     1.0f,  1.0f, -1.0f,
-    -1.0f,  1.0f, -1.0f,
+static constexpr float CubeVertices[] =
+{
+	-1.0f,  1.0f, -1.0f,
+	-1.0f, -1.0f, -1.0f,
+	1.0f, -1.0f, -1.0f,
+	1.0f, -1.0f, -1.0f,
+	1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,
 
-    -1.0f, -1.0f,  1.0f,
-    -1.0f, -1.0f, -1.0f,
-    -1.0f,  1.0f, -1.0f,
-    -1.0f,  1.0f, -1.0f,
-    -1.0f,  1.0f,  1.0f,
-    -1.0f, -1.0f,  1.0f,
+	-1.0f, -1.0f,  1.0f,
+	-1.0f, -1.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f,  1.0f,
+	-1.0f, -1.0f,  1.0f,
 
-     1.0f, -1.0f, -1.0f,
-     1.0f, -1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f, -1.0f,
-     1.0f, -1.0f, -1.0f,
+	1.0f, -1.0f, -1.0f,
+	1.0f, -1.0f,  1.0f,
+	1.0f,  1.0f,  1.0f,
+	1.0f,  1.0f,  1.0f,
+	1.0f,  1.0f, -1.0f,
+	1.0f, -1.0f, -1.0f,
 
-    -1.0f, -1.0f,  1.0f,
-    -1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f, -1.0f,  1.0f,
-    -1.0f, -1.0f,  1.0f,
+	-1.0f, -1.0f,  1.0f,
+	-1.0f,  1.0f,  1.0f,
+	1.0f,  1.0f,  1.0f,
+	1.0f,  1.0f,  1.0f,
+	1.0f, -1.0f,  1.0f,
+	-1.0f, -1.0f,  1.0f,
 
-    -1.0f,  1.0f, -1.0f,
-     1.0f,  1.0f, -1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-    -1.0f,  1.0f,  1.0f,
-    -1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,
+	1.0f,  1.0f, -1.0f,
+	1.0f,  1.0f,  1.0f,
+	1.0f,  1.0f,  1.0f,
+	-1.0f,  1.0f,  1.0f,
+	-1.0f,  1.0f, -1.0f,
 
-    -1.0f, -1.0f, -1.0f,
-    -1.0f, -1.0f,  1.0f,
-     1.0f, -1.0f, -1.0f,
-     1.0f, -1.0f, -1.0f,
-    -1.0f, -1.0f,  1.0f,
-     1.0f, -1.0f,  1.0f
+	-1.0f, -1.0f, -1.0f,
+	-1.0f, -1.0f,  1.0f,
+	1.0f, -1.0f, -1.0f,
+	1.0f, -1.0f, -1.0f,
+	-1.0f, -1.0f,  1.0f,
+	1.0f, -1.0f,  1.0f
+};
+
+struct GeneratedMesh
+{
+	Vector<float3> vertices;
+	Vector<uint3> indices;
+};
+static GeneratedMesh GenerateIcosphere(uint32_t subdivideLevel = 2)
+{
+	const static float t = (1.0 + static_cast<float>(sqrt(5.0))) / 2.0;
+	const static float IcosahedronVertices[] =
+	{
+		-1.0f, t, 0.0f,
+		1.0f, t, 0.0f,
+		-1.0f, -t, 0.0f,
+		1.0f, -t, 0.0f,
+
+		0.0f, -1.0f, t,
+		0.0f, 1.0f, t,
+		0.0f, -1.0f, -t,
+		0.0f, 1.0f, -t,
+
+		t, 0.0f, -1.0f,
+		t, 0.0f, 1.0f,
+		-t, 0.0f, -1.0f,
+		-t, 0.0f, 1.0f,
+	};
+
+	Vector<float3> vertices;
+	vertices.reserve(12);
+	for (uint32_t i = 0; i < 12; ++i)
+	{
+		float v0 = IcosahedronVertices[i * 3];
+		float v1 = IcosahedronVertices[i * 3 + 1];
+		float v2 = IcosahedronVertices[i * 3 + 2];
+		float length = static_cast<float>(sqrt(v0 * v0 + v1 * v1 + v2 * v2));
+		vertices.push_back(float3{ v0 / length, v1 / length , v2 / length  });
+	}
+
+	static constexpr uint32_t IcosahedronIndices[] =
+	{
+		0, 11, 5,
+		0, 5, 1,
+		0, 1, 7,
+		0, 7, 10,
+		0, 10, 11,
+
+		// 5 adjacent faces
+		1, 5, 9,
+		5, 11, 4,
+		11, 10, 2,
+		10, 7, 6,
+		7, 1, 8,
+
+		// 5 faces around point 3
+		3, 9, 4,
+		3, 4, 2,
+		3, 2, 6,
+		3, 6, 8,
+		3, 8, 9,
+
+		// 5 adjacent faces
+		4, 9, 5,
+		2, 4, 11,
+		6, 2, 10,
+		8, 6, 7,
+		9, 8, 1,
+	};
+	Vector<uint3> indices;
+	indices.reserve(20);
+	for (uint32_t i = 0; i < 20; i++)
+	{
+		indices.push_back(uint3{ IcosahedronIndices[i * 3], IcosahedronIndices[i * 3 + 1], IcosahedronIndices[i * 3 + 2] });
+	}
+
+	// refine triangles
+	Vector<float3> finalVertices;
+	Vector<uint3> finalIndices;
+	for (uint32_t level = 0; level < subdivideLevel; ++level)
+	{
+		const uint32_t oldSize = static_cast<uint32_t>(vertices.size());
+		for (const uint3& tri : indices)
+		{
+			float3 v0 = vertices[tri.x];
+			float3 v1 = vertices[tri.y];
+			float3 v2 = vertices[tri.z];
+
+			uint32_t i0 = static_cast<uint32_t>(vertices.size());
+			vertices.push_back(float3{ (v0.x + v1.x) / 2.0f, (v0.y + v1.y) / 2.0f, (v0.z + v1.z) / 2.0f });
+			uint32_t i1 = (i0 + 1); 
+			vertices.push_back(float3{ (v1.x + v2.x) / 2.0f, (v1.y + v2.y) / 2.0f, (v1.z + v2.z) / 2.0f });
+			uint32_t i2 = (i1 + 1);
+			vertices.push_back(float3{ (v2.x + v0.x) / 2.0f, (v2.y + v0.y) / 2.0f, (v2.z + v0.z) / 2.0f });
+
+			finalIndices.push_back({ tri.x , i0, i2});
+			finalIndices.push_back({ tri.y , i1, i0});
+			finalIndices.push_back({ tri.z , i2, i1});
+			finalIndices.push_back({ i0, i1, i2});
+		}
+		indices = std::move(finalIndices);
+		finalIndices.clear();
+		for (uint32_t i = oldSize; i < vertices.size(); ++i)
+		{
+			const auto& vertex = vertices[i];
+			float length = static_cast<float>(sqrt(vertex.x * vertex.x + vertex.y * vertex.y + vertex.z * vertex.z));
+			vertices[i] = float3{ vertex.x / length, vertex.y / length, vertex.z / length };
+		}
+	}
+
+	return GeneratedMesh{ std::move(vertices), std::move(indices) };
 };
 };
 
--- Sample/Source/SampleModel.cpp
+++ Sample/Source/SampleModel.cpp
@@ -3,6 +3,7 @@
 #include <DERendering/DataType/GraphicsDataType.h>
 #include <DERendering/DataType/LightType.h>
 #include <DERendering/Imgui/imgui.h>
+#include <DERendering/RenderConstant.h>
 #include <DEGame/Renderer/Renderer.h>
 #include <DEGame/Loader/SceneLoader.h>
 #include <DEGame/Loader/TextureLoader.h>
@@ -17,13 +18,31 @@ void SampleModel::Setup(Renderer* renderer)
 	sceneLoader.Load("SampleScene", scene);
 	
 	// Add lights manually
+	for (uint32_t i = 0; i < 8; ++i)
 	{
 		auto& light = PointLight::Create();
 		scene.Add(light);
-		light.position = float3{ 0.0f, 5.0f, 0.0f };
-		light.color = float3{ 1.0f, 1.0f, 1.0f };
-		light.intensity = 1.0f;
+		light.position = float3{ -16.0f + i * 4, 5.0f, 0.0f };
+		light.color = float3{ static_cast<float>(i % 2), 1.0f, 1.0f };
+		light.falloffRadius = 3.0f;
+		light.intensity = 10.0f;
 		light.enable = true;
+
+		Mesh& mesh = Mesh::Create();
+		light.debugMesh = mesh.Index();
+		mesh.scale = light.falloffRadius;
+		mesh.translate = light.position;
+		auto icosphere = RenderConstant::GenerateIcosphere();
+		{
+			mesh.m_Vertices.Init(renderer->GetRenderDevice()->m_Device, sizeof(float3), icosphere.vertices.size() * sizeof(float3));
+			mesh.m_Vertices.Update(icosphere.vertices.data(), icosphere.vertices.size() * sizeof(float3));
+			mesh.m_iNumVertices = icosphere.vertices.size();
+		}
+		{
+			mesh.m_Indices.Init(renderer->GetRenderDevice()->m_Device, sizeof(uint32_t), icosphere.indices.size() * sizeof(uint3));
+			mesh.m_Indices.Update(icosphere.indices.data(), icosphere.indices.size() * sizeof(uint3));
+			mesh.m_iNumIndices = icosphere.indices.size() * 3;
+		}
 	}
 	{
 		QuadLight& light = QuadLight::Create();
@@ -34,10 +53,10 @@ void SampleModel::Setup(Renderer* renderer)
 		light.color = float3{ 1.0f, 1.0f, 1.0f };
 		light.intensity = 1.0f;
 		light.enable = true;
+		light.falloffRadius = 100.0f;
 
 		Mesh& mesh = Mesh::Create();
 		light.mesh = mesh.Index();
-		scene.Add(mesh);
 		{
 			float posX = light.position.x + light.width / 2.0f;
 			float posY = light.position.y + light.height / 2.0f;
@@ -101,63 +120,96 @@ void SampleModel::SetupUI(float dt)
 	auto& scene = m_pRenderer->GetScene();
 	bool active;
 	ImGui::Begin("Edit", &active);
-	scene.ForEach<Mesh>([&](Mesh& mesh) {
-		char name[256];
-		sprintf_s(name, "mesh_%i", mesh.Index());
-		if (ImGui::CollapsingHeader(name))
-		{
-			auto &material = Material::Get(mesh.m_MaterialID);
-			sprintf_s(name, "mat_%i", mesh.m_MaterialID);
+	if (ImGui::CollapsingHeader("Mesh"))
+	{
+		scene.ForEach<Mesh>([&](Mesh& mesh) {
+			char name[256];
+			sprintf_s(name, "mesh_%i", mesh.Index());
 			if (ImGui::TreeNode(name))
 			{
-				ImGui::SliderFloat3("Albedo", &material.albedo.x, 0.0f, 1.0f);
-				ImGui::SliderFloat("Roughness", &material.roughness, 0.0f, 1.0f);
-				ImGui::SliderFloat("Metallic", &material.metallic, 0.0f, 1.0f);
-				ImGui::SliderFloat("AO", &material.ao, 0.0f, 1.0f);
+				auto &material = Material::Get(mesh.m_MaterialID);
+				sprintf_s(name, "mat_%i", mesh.m_MaterialID);
+				if (ImGui::TreeNode(name))
+				{
+					ImGui::SliderFloat3("Albedo", &material.albedo.x, 0.0f, 1.0f);
+					ImGui::SliderFloat("Roughness", &material.roughness, 0.0f, 1.0f);
+					ImGui::SliderFloat("Metallic", &material.metallic, 0.0f, 1.0f);
+					ImGui::SliderFloat("AO", &material.ao, 0.0f, 1.0f);
+					ImGui::TreePop();
+				}
 				ImGui::TreePop();
 			}
-		}
-	});
-	scene.ForEach<PointLight>([&](PointLight& light) {
-		char name[256];
-		sprintf_s(name, "pointLight_%i", light.Index());
-		if (ImGui::CollapsingHeader(name))
-		{
-			ImGui::Checkbox("Enable", &light.enable);
-			ImGui::SliderFloat3("Position", &light.position.x, -50.0f, 50.0f);
-			ImGui::SliderFloat3("Color", &light.color.x, 0.0f, 1.0f);
-			ImGui::SliderFloat("Intensity", &light.intensity, 0.0f, 10.0f);
-		}
-	});
-	scene.ForEach<QuadLight>([&](QuadLight& light) {
-		char name[256];
-		sprintf_s(name, "quadLight_%i", light.Index());
-		if (ImGui::CollapsingHeader(name))
-		{
-			ImGui::Checkbox("Enable", &light.enable);
-			ImGui::SliderFloat3("Position", &light.position.x, -50.0f, 50.0f);
-			ImGui::SliderFloat("Width", &light.width, 0.0f, 50.0f);
-			ImGui::SliderFloat("Height", &light.height, 0.0f, 50.0f);
-			ImGui::SliderFloat3("Color", &light.color.x, 0.0f, 1.0f);
-			ImGui::SliderFloat("Intensity", &light.intensity, 0.0f, 10.0f);
+		});
+	}
+	if (ImGui::CollapsingHeader("PointLight"))
+	{
+		scene.ForEach<PointLight>([&](PointLight& light) {
+			char name[256];
+			sprintf_s(name, "pointLight_%i", light.Index());
+			if (ImGui::TreeNode(name))
+			{
+				ImGui::Checkbox("Enable", &light.enable);
+				ImGui::SliderFloat3("Position", &light.position.x, -50.0f, 50.0f);
+				ImGui::SliderFloat3("Color", &light.color.x, 0.0f, 1.0f);
+				ImGui::SliderFloat("Intensity", &light.intensity, 0.0f, 100.0f);
+				ImGui::SliderFloat("Falloff Radius", &light.falloffRadius, 0.0f, 200.0f);
+				ImGui::Checkbox("Debug", &light.debug);
+				ImGui::TreePop();
 
-			float posX = light.position.x + light.width / 2.0f;
-			float posY = light.position.y + light.height / 2.0f;
-			float z = light.position.z;
-			float negX = light.position.x - light.width / 2.0f;
-			float negY = light.position.y - light.height / 2.0f;
-			float vertices[] = {
-				negX, posY, z,
-				posX, posY, z,
-				posX, negY, z,
-				negX, negY, z
-			};
-			Mesh& mesh = Mesh::Get(light.mesh);
-			mesh.m_Vertices.Update(vertices, sizeof(vertices));
-			Material& material = Material::Get(mesh.m_MaterialID);
-			material.albedo = float3{ light.intensity, light.intensity, light.intensity };
+				Mesh& mesh = Mesh::Get(light.debugMesh);
+				mesh.translate = light.position;
+				mesh.scale = light.falloffRadius;
+			}
+		});
+		if (ImGui::Button("All debug shape ON"))
+		{
+			scene.ForEach<PointLight>([&](PointLight& light) {
+				light.debug = true;
+			});
+		}		
+		if (ImGui::Button("All debug shape OFF"))
+		{
+			scene.ForEach<PointLight>([&](PointLight& light) {
+				light.debug = false;
+			});
 		}
-	});
+
+	}
+	if (ImGui::CollapsingHeader("QuadLight"))
+	{
+		scene.ForEach<QuadLight>([&](QuadLight& light) {
+			char name[256];
+			sprintf_s(name, "quadLight_%i", light.Index());
+			if (ImGui::TreeNode(name))
+			{
+				ImGui::Checkbox("Enable", &light.enable);
+				ImGui::SliderFloat3("Position", &light.position.x, -50.0f, 50.0f);
+				ImGui::SliderFloat("Width", &light.width, 0.0f, 50.0f);
+				ImGui::SliderFloat("Height", &light.height, 0.0f, 50.0f);
+				ImGui::SliderFloat3("Color", &light.color.x, 0.0f, 1.0f);
+				ImGui::SliderFloat("Intensity", &light.intensity, 0.0f, 100.0f);
+				ImGui::SliderFloat("Falloff Radius", &light.falloffRadius, 0.0f, 200.0f);
+				ImGui::Checkbox("Debug", &light.debug);
+
+				float posX = light.position.x + light.width / 2.0f;
+				float posY = light.position.y + light.height / 2.0f;
+				float z = light.position.z;
+				float negX = light.position.x - light.width / 2.0f;
+				float negY = light.position.y - light.height / 2.0f;
+				float vertices[] = {
+					negX, posY, z,
+					posX, posY, z,
+					posX, negY, z,
+					negX, negY, z
+				};
+				Mesh& mesh = Mesh::Get(light.mesh);
+				mesh.m_Vertices.Update(vertices, sizeof(vertices));
+				Material& material = Material::Get(mesh.m_MaterialID);
+				material.albedo = float3{ light.intensity, light.intensity, light.intensity };
+				ImGui::TreePop();
+			}
+		});
+	}
 
 	ImGui::End();
 }
